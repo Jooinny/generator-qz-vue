f5792d38ccaf9a607e1356bb4f2d6af9d8f59a99
Ably <jooinny_boyzz@163.com>
2021-06-05 11:27:22 +0800

    feat: initial of generator-qz-vue

---
+++ app/index.js
@@ -0,0 +1,53 @@
+const Generator = require("yeoman-generator")
+
+module.exports = class extends Generator {
+    prompting() {
+        return this.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Your project name",
+                default: this.appname
+            }
+        ]).then(answers => {
+            this.answers = answers
+        })
+    }
+    writing() {
+        const templates = [
+            '.browserslistrc',
+            '.editorconfig',
+            '.env.development',
+            '.env.production',
+            '.eslintrc.js',
+            '.gitignore',
+            'babel.config.js',
+            'package.json',
+            'postcss.config.js',
+            'README.md',
+            'public/favicon.ico',
+            'public/index.html',
+            'src/App.vue',
+            'src/main.js',
+            'src/router.js',
+            'src/assets/logo.png',
+            'src/components/HelloWorld.vue',
+            'src/store/actions.js',
+            'src/store/getters.js',
+            'src/store/index.js',
+            'src/store/mutations.js',
+            'src/store/state.js',
+            'src/utils/request.js',
+            'src/views/About.vue',
+            'src/views/Home.vue'
+        ]
+        templates.forEach(item => {
+            // 源模板文件
+            const tmpl = this.templatePath(item)
+            // 目标生成文件
+            const target = this.destinationPath(item)
+            // 模板拷贝
+            this.fs.copyTpl(tmpl, target, this.answers)
+        })
+    }
+}
